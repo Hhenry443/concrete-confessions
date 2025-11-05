b0b4821fac4e8a90eea6e2058f6d72d8a38fe663
Henry Barnes <henrytbarnes07@gmail.com>
2025-11-05 14:20:50 +0000

    added ddev and basic db connection

--- public/index.php
+++ public/index.php
@@ -1,3 +1,28 @@
+<?php
+
+$servername = "db";
+$username = "test";
+$password = "test";
+$dbname = "concrete_confessions";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM test_data";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  echo '<pre>' . var_export($result->fetch_assoc(), true) . '</pre>';
+} else {
+  echo "0 results";
+}
+$conn->close();
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -5,11 +30,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Concrete Confessions</title>
-  <link href="./public/css/output.css" rel="stylesheet">
+  <link href="/css/output.css" rel="stylesheet">
 </head>
 
 <body>
-  <p>Hello World</p>
+  <p>Confess your sins UEA...</p>
 </body>
 
 </html>
